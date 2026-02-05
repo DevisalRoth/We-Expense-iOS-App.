ced5ea21633116535637f677192952d41ab63e6f
Visalroth <chouvisalroth9999@gmail.com>
2026-02-05 14:36:40 +0700

    Initial Commit

---
+++ ExpendAppWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpendAppWithSwiftUI
+//
+//  Created by Visalroth on 5/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpendAppWithSwiftUI/ExpendAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpendAppWithSwiftUIApp.swift
+//  ExpendAppWithSwiftUI
+//
+//  Created by Visalroth on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpendAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
